e0c90f9e91a6a7ea575559ef9de5eb6b13169a61
Anil-Gollapalli <anilgollapalli2@gmail.com>
2024-11-19 17:19:10 +0530

    Safe commit message

--- src/menuTab.js
+++ src/menuTab.js
@@ -4,15 +4,15 @@ export default function menuTab() {
     <h1>Our Menu</h1>
     <!-- Menu Dish Image -->
     <div class="menu-item">
-      <img src="../images/burger.jpg" alt="Burger">
+      <img src="./images/burger.jpg" alt="Burger">
       <p>Burger</p>
     </div>
     <div class="menu-item">
-      <img src="../images/roti.jpg" alt="Roti">
+      <img src="./images/roti.jpg" alt="Roti">
       <p>Roti with kurma</p>
     </div>
     <div class="menu-item">
-      <img src="../images/pizzaa.jpg" alt="Pizzaa">
+      <img src="./images/pizzaa.jpg" alt="Pizzaa">
       <p>Pizzaaa</p>
     </div>
   `;
