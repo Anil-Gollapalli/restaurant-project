095ff3055af576321a2bad5860d7e00a04d0f096
Anil-Gollapalli <anilgollapalli2@gmail.com>
2024-11-20 13:07:54 +0530

    Updates

--- src/homeTab.js
+++
@@ -1,9 +0,0 @@
-export default function homeTab() {
-  const div = document.createElement('div');
-  div.innerHTML = `
-    <h1>Welcome to Anil's Restaurant!</h1>
-    <img src="./images/chef.jpg" alt="Restaurant Dish">
-    <p><marquee>Enjoy our delicious meals and great service.</marquee></p>
-  `;
-  return div;
-}
