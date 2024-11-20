abb8e488bf69d92ed75fc798220dee72a1420a3a
Anil-Gollapalli <anilgollapalli2@gmail.com>
2024-11-20 12:54:15 +0530

    Safe commit message

--- src/contactTab.js
+++ src/contactTab.js
@@ -2,7 +2,8 @@ export default function contactTab() {
     const div = document.createElement('div');
     div.innerHTML = `
       <h1>Contact Us</h1>
-      <p>Call us at 123-456-7890 or email us at contact@restaurant.com</p>
+      <img src="./images/bar-food.jpg" alt="Restaurant Dish">
+      <p>Call us at 8374630638 or email us at Anil@restaurant.com</p>
     `;
     return div;
   }
